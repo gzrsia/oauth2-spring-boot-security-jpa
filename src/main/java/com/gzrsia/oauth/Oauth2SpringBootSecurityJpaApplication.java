b4c0490c883873f6df61703d2b8d5034940019c1
Letisia Pangata'a <lpan375@aucklanduni.ac.nz>
2019-11-29 15:58:20 +1300

    Initial commit

---
+++ src/main/java/com/gzrsia/oauth/Oauth2SpringBootSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.gzrsia.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SpringBootSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SpringBootSecurityJpaApplication.class, args);
+	}
+
+}
